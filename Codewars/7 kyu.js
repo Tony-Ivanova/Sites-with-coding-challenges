56d54becaccfd94c077786059ff0c839664a8914
Tony Ivanova <102296693+Tony-Ivanova@users.noreply.github.com>
2022-05-23 19:52:55 +0300

    Update 7 kyu.js

--- Codewars/7 kyu.js
+++ Codewars/7 kyu.js
@@ -149,3 +149,89 @@ String.prototype.toJadenCase = function () {
               .map(word => word.charAt(0).toUpperCase() + word.slice(1))
               .join(' ')
 };
+
+// Barista problem
+function barista(coffees){
+    if(coffees.length == 0){
+      return 0;
+    }
+  
+  let sum = 0
+    
+    coffees.sort((a, b) => a-b)
+                  .reduce((acc, value, index) => {
+                        acc = acc + 2 + value
+                        sum += acc
+                        return acc
+                  })
+  
+  return sum + coffees[0]
+}
+
+// Digital cypher
+function encode(str,  n)
+{
+  let cypher = n.toString().split('').map(Number)
+  
+  let dictionary = {
+    'a' : 1,
+    'b' : 2,
+    'c' : 3,
+    'd' : 4,
+    'e' : 5,
+    'f' : 6,
+    'g' : 7,
+    'h' : 8,
+    'i' : 9,
+    'j' : 10,
+    'k' : 11,
+    'l' : 12,
+    'm' : 13,
+    'n' : 14,
+    'o' : 15,
+    'p' : 16,
+    'q' : 17,
+    'r' : 18,
+    's' : 19,
+    't' : 20,
+    'u' : 21,
+    'v' : 22,
+    'w' : 23,
+    'x' : 24,
+    'y' : 25,
+    'z' : 26,
+  }
+  
+  let arr = str.split('').map(x => dictionary[x])
+  
+  return arr.map(x => {
+      let forCrypt = cypher.shift()
+      cypher.push(forCrypt)
+      return x+forCrypt
+  })
+}
+
+// Regex validate PIN code
+function validatePIN (pin) {
+  return /^[0-9]{4}$/.test(pin) || /^[0-9]{6}$/.test(pin)
+}
+
+// Mumbling
+function accum(s) {
+   return s.toLowerCase().split('').reduce((acc, value, index) => {
+      
+     let currentIteration = Array(index+1) //the size of the array
+                            .fill(value) //fill it with the value
+                            .join('')    //turn it into string
+     
+      return acc += (currentIteration.charAt().toUpperCase() 
+                     + currentIteration.slice(1)) 
+                     + '-' //concatenate the firstChar to Uppercase + the rest of the string + -
+                
+   }, []).slice(0, -1) //remove the last -
+}
+
+// Odd or Even?
+function oddOrEven(array) {
+    return array.reduce((a, b) => a+b, 0) % 2 === 0 ? 'even':'odd'
+}
