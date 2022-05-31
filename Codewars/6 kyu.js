ec4efec8f7c8f4ec7cbcb7493ee8b00e9d84685c
Tony Ivanova <102296693+Tony-Ivanova@users.noreply.github.com>
2022-05-31 23:41:18 +0300

    Update 6 kyu.js

--- Codewars/6 kyu.js
+++ Codewars/6 kyu.js
@@ -101,3 +101,17 @@ function comp(array1, array2){
   
   return array1.map(v => v * v).every((v, i) => v == array2[i]);
 }
+
+//Who likes it?
+function likes(names) {
+   if(!names.length)
+    return 'no one likes this'
+   if(names.length === 1)
+    return `${names[0]} likes this`
+   if(names.length === 2)
+    return `${names[0]} and ${names[1]} like this`
+   if(names.length === 3)
+    return `${names[0]}, ${names[1]} and ${names[2]} like this`
+
+    return `${names[0]}, ${names[1]} and ${names.length - 2} others like this`
+}
