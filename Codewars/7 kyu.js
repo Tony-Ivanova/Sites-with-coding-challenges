19d3b6c650966c8d51d1d522f4332e648083b5c5
Tony Ivanova <102296693+Tony-Ivanova@users.noreply.github.com>
2022-06-27 23:56:28 +0300

    Update 7 kyu.js

--- Codewars/7 kyu.js
+++ Codewars/7 kyu.js
@@ -252,3 +252,18 @@ function redistributeWealth(wealth) {
   
   let newArr =  wealth.fill(wealthPerCap);
 }
+
+
+//Baby shark lyrics generator
+function babySharkLyrics(){
+  const family = ['Baby shark','Mommy shark','Daddy shark','Grandma shark','Grandpa shark',"Let's go hunt"];
+  
+  let str = '';
+  let doo = ' doo'.repeat(6);
+  
+  family.map(f => {
+    str += `${f},${doo}\n`.repeat(3)+`${f}!\n`
+  })
+  str += 'Run away,…'
+  return str;
+}
